efec03b2b0c8fb720b8e243e5fe956ca0c801d12
Mark Klimov <markklimoff@gmail.com>
2024-08-09 21:12:29 +0500

    change logic routing, delete useless file, add morkdowm Readme

--- src/main.py
+++ src/main.py
@@ -1,43 +1,45 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, requests
 from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse, Response
 from fastapi import HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import httpx
 
 
 app = FastAPI()
 
 
-app.mount('/static', StaticFiles(directory='static'), name='static')
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory='templates')
+templates = Jinja2Templates(directory="templates")
 
 
 # Оотображает раздел с информацией о компании
-@app.get('/', response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 async def get_company_about(request: Request):
-    return templates.TemplateResponse(request=request, name='company-about.html')
+    return templates.TemplateResponse(request=request, name="company-about.html")
 
 
 # Отображает раздел с информацией о сотрудничестве с компанией
-@app.get('/company-cooperation', response_class=HTMLResponse)
+@app.get("/company-cooperation", response_class=HTMLResponse)
 async def get_company_cooperation(request: Request):
-    return templates.TemplateResponse(request=request, name='company-cooperation.html')
-
-
-# Отображает раздел с часто задаваемыми вопросами и ответами на них
-@app.get('/user-help', response_class=HTMLResponse)
-async def get_user_help(request: Request):
-    return templates.TemplateResponse(request=request, name='user-help-get.html')
-
-
-# Отображает раздел аннотации фото (заглушка)
-@app.get('/video/annotation-photo', response_class=HTMLResponse)
-async def get_photo_annotation(request: Request):
-    return templates.TemplateResponse(request=request, name='annotation-photo.html')
+    return templates.TemplateResponse(request=request, name="company-cooperation.html")
 
 
 # Отображает раздел с аннотацией видео
-@app.get('/video/annotation-video', response_class=HTMLResponse)
+@app.get("/video/annotation-video", response_class=HTMLResponse)
 async def get_video_annotation(request: Request):
-    return templates.TemplateResponse(request=request, name='annotation-video.html')
+    return templates.TemplateResponse(request=request, name="annotation-video.html")
+
+
+@app.get("/get_markdown")
+async def get_markdown():
+    markdown_url = (
+        "https://raw.githubusercontent.com/lnikioffic/VisionDataForge/master/README.md"
+    )
+    async with httpx.AsyncClient() as client:
+        response = await client.get(markdown_url)
+        if response.status_code == 200:
+            return {"content": response.text}
+        else:
+            return {"error": "Failed to fetch markdown file"}, 500
